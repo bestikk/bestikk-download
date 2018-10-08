0722b5c4008f7becd2b3247f54e36ba7a5d6fe58
Guillaume Grossetie <ggrossetie@gmail.com>
2018-10-08 20:11:34 +0200

    Major update
    - Node 8.11+
    - Update dependencies
    - Follow redirect
    - Use Promise/Async (instead of callback)

--- index.js
+++ index.js
@@ -1,27 +1,61 @@
-var log = require('bestikk-log');
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
+const log = require('bestikk-log');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
-var Download = function() {
+const Download = function (opts) {
+  this.followRedirect = (opts.followRedirect !== undefined) ? opts.followRedirect : true;
+  this.maxRedirects = (opts.maxRedirects !== undefined) ? opts.maxRedirects : 10;
 };
 
-Download.prototype.getContentFromURL = function (source, target, callback) {
-  log.transform('get', source, target);
-  var targetStream = fs.createWriteStream(target);
-  var downloadModule;
-  // startWith alternative
-  if (source.lastIndexOf('https', 0) === 0) {
+const hasHeader = function (header, headers) {
+  let headersKey = Object.keys(headers)
+  const headersLowerCase = headersKey.map(function (h) {
+    return h.toLowerCase();
+  });
+  header = header.toLowerCase();
+  for (let i = 0; i < headersLowerCase.length; i++) {
+    if (headersLowerCase[i] === header) return headersKey[i];
+  }
+  return false;
+};
+
+Download.prototype._download = function (location, targetStream, redirectsCount) {
+  let downloadModule;
+  if (location.startsWith('https')) {
     downloadModule = https;
   } else {
     downloadModule = http;
   }
-  downloadModule.get(source, function (response) {
-    response.pipe(targetStream);
-    targetStream.on('finish', function () {
-      targetStream.close(callback);
+  const followRedirect = this.followRedirect;
+  const maxRedirects = this.maxRedirects;
+  const _download = this._download;
+  downloadModule.get(location, function (response) {
+    if (response.statusCode >= 300 && response.statusCode < 400 && hasHeader('location', response.headers)) {
+      const location = response.headers[hasHeader('location', response.headers)];
+      if (followRedirect && redirectsCount < maxRedirects) {
+        _download(location, targetStream, redirectsCount + 1);
+      } else {
+        response.pipe(targetStream);
+      }
+    } else {
+      response.pipe(targetStream);
+    }
+  });
+}
+
+Download.prototype.getContentFromURL = async function (source, target) {
+  log.transform('get', source, target);
+  const targetStream = fs.createWriteStream(target);
+  this._download(source, targetStream, 0);
+  targetStream.on('finish', function () {
+    targetStream.close();
+  });
+  return new Promise(function(resolve) {
+    targetStream.on('close', function () {
+      resolve();
     });
   });
 };
 
-module.exports = new Download();
+module.exports = Download;
--- spec/spec.js
+++ spec/spec.js
@@ -1,14 +1,14 @@
-var fs = require('fs');
-var path = require('path');
-var os = require('os');
-var chai = require('chai');
-var bfs = require('bestikk-fs');
-var assert = chai.assert;
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+const chai = require('chai');
+const bfs = require('bestikk-fs');
+const assert = chai.assert;
 
-var main = require('../index.js');
+const Module = require('../index.js');
 
-describe('Bestikk', function() {
-  var TEST_DIR;
+describe('Bestikk', function () {
+  let TEST_DIR;
 
   beforeEach(function () {
     TEST_DIR = path.join(os.tmpdir(), 'test', 'bestikk-download');
@@ -19,17 +19,40 @@ describe('Bestikk', function() {
     bfs.removeSync(TEST_DIR);
   });
 
-  describe('+ getContentFromURL()', function () {
+  describe('+ getContentFromURL()', () => {
     it('should get content from HTTPS', function (done) {
-      var dest = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random());
+      const dest = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random());
       bfs.mkdirsSync(dest);
-      var license = path.join(dest, 'LICENSE');
-      main.getContentFromURL('https://raw.githubusercontent.com/bestikk/bestikk-log/master/LICENSE', license, function() {
+      const license = path.join(dest, 'LICENSE');
+      new Module({}).getContentFromURL('https://raw.githubusercontent.com/bestikk/bestikk-log/master/LICENSE', license).then(() => {
         assert(fs.existsSync(license));
-        assert.include(fs.readFileSync(license, {encoding: 'utf8'}), 'bestikk');
+        assert.include(fs.readFileSync(license, { encoding: 'utf8' }), 'bestikk');
         done();
       });
     });
+    it('should follow redirect', function(done) {
+      const dest = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random());
+      bfs.mkdirsSync(dest);
+      const license = path.join(dest, 'LICENSE');
+      new Module({}).getContentFromURL('https://github.com/bestikk/bestikk-download/releases/download/1.0.0-rc1/LICENSE', license).then(() => {
+        assert(fs.existsSync(license));
+        assert.include(fs.readFileSync(license, { encoding: 'utf8' }), 'bestikk');
+        done();
+      });
+    })
+    it('should not follow redirect', function(done) {
+      const dest = path.join(TEST_DIR, 'tmp-' + Date.now() + Math.random());
+      bfs.mkdirsSync(dest);
+      const license = path.join(dest, 'LICENSE');
+      new Module({
+        followRedirect: false,
+        maxRedirects: 10
+      }).getContentFromURL('https://github.com/bestikk/bestikk-download/releases/download/1.0.0-rc1/LICENSE', license).then(() => {
+        assert(fs.existsSync(license));
+        assert.include(fs.readFileSync(license, { encoding: 'utf8' }), 'redirected');
+        done();
+      });
+    })
   });
 
 });
